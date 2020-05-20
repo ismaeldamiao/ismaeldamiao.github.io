33edd3d7d849822624999c0eb3ae0e703052552e
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2020-05-20 13:36:30 -0300

    Create LaTeX.js

---
+++ assets/js/LaTeX.js
@@ -0,0 +1,9 @@
+MathJax.Hub.Config({
+   extensions: [&quot;tex2jax.js&quot;,&quot;TeX/AMSmath.js&quot;,&quot;TeX/AMSsymbols.js&quot;],
+   jax: [&quot;input/TeX&quot;, &quot;output/HTML-CSS&quot;],
+   tex2jax: {
+   inlineMath: [ [&#39;$&#39;,&#39;$&#39;], [&quot;\\(&quot;,&quot;\\)&quot;] ],
+   displayMath: [ [&#39;$$&#39;,&#39;$$&#39;], [&quot;\\[&quot;,&quot;\\]&quot;] ],
+   },
+   &quot;HTML-CSS&quot;: { availableFonts: [&quot;TeX&quot;] }
+});
