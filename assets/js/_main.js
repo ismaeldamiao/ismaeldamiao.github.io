c130d5ff186b31d58d88fe7cda5336e1e396921e
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2020-05-20 13:36:05 -0300

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,16 +2,6 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
-MathJax.Hub.Config({
-   extensions: [&quot;tex2jax.js&quot;,&quot;TeX/AMSmath.js&quot;,&quot;TeX/AMSsymbols.js&quot;],
-   jax: [&quot;input/TeX&quot;, &quot;output/HTML-CSS&quot;],
-   tex2jax: {
-   inlineMath: [ [&#39;$&#39;,&#39;$&#39;], [&quot;\\(&quot;,&quot;\\)&quot;] ],
-   displayMath: [ [&#39;$$&#39;,&#39;$$&#39;], [&quot;\\[&quot;,&quot;\\]&quot;] ],
-   },
-   &quot;HTML-CSS&quot;: { availableFonts: [&quot;TeX&quot;] }
-});
-
 $(document).ready(function(){
    // Sticky footer
   var bumpIt = function() {
