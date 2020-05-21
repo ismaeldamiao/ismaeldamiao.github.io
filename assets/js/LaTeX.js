27f13170467feaf0384e3e108e65534619d20c66
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2020-05-20 23:51:32 -0300

    Delete LaTeX.js

--- assets/js/LaTeX.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-   extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js"],
-   jax: ["input/TeX", "output/HTML-CSS"],
-   tex2jax: {
-   inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-   displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
-   },
-   "HTML-CSS": { availableFonts: ["TeX"] }
-});
